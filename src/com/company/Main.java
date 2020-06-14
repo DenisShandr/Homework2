2dc02e6109e4bad36bcedb395d15316280cc4578
DenisShabdr <paronix726>
2020-06-14 19:34:44 +0300

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 0;
+        for (int i = 1; i <= 10; i++) {
+            n = n + i;
+        }
+        System.out.println("Ответ: " + n);
+    }
+}

602f15434cc28cc1e2c8dcb7051a3da76c4d648f
DenisShabdr <paronix726>
2020-06-29 11:00:13 +0300

    Homework_2 Task_1

--- src/com/company/Task_1.java
+++ src/com/company/Task_1.java
@@ -1,10 +1,41 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Task_1 {
 
     public static void main(String[] args) {
-
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размерность: ");
+        int size;
+        size = in.nextInt();
+        int layer = 1;
+        if (size % 2 != 1) {
+            size++;
+        }
+        int[][] array = new int[size][size];
+        for (int i = 0; i < array.length / 2 + 1; i++) {
+            for (int j = 0; j < array[i].length / 2 + 1; j++) {
+                if (i == j) {
+                    for (int k = i; k < array.length; k++) {
+                        for (int l = j; l < array.length - k; l++) {
+                            array[k][l] = layer;
+                            if (l == array.length - k - 1) {
+                                array[k][array.length - layer] = layer;
+                                array[array.length - layer][k] = layer;
+                            }
+                        }
+                    }
+                }
+            }
+            layer++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
